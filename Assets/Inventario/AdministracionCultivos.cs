984879685fd301920e939252cd84ba82cf24dd62
gus238 <gus.gilardoni@hotmail.com>
2024-10-26 21:22:59 -0300

    Sincronizacion GUI Cultivos e Inventario
    Aun hay algunos fallos como que al apretar volver en cultivos tenes que apretar 2 veces tab en el inventario para cerrarlo y al apretar tab en cultivos vuelve a inventario en vez de salir de todo

--- Assets/Inventario/AdministracionCultivos.cs
+++ Assets/Inventario/AdministracionCultivos.cs
@@ -4,16 +4,67 @@ using UnityEngine;
 
 public class AdministracionCultivos : MonoBehaviour
 {
-    public GameObject Cultivos;
-    public bool ActivacionCultivos;
+    public GameObject MenuCultivos;
+    public bool InterfazCultivos;
+    public AdministracionInventario adminInventario;
+
     void Start()
     {
-        
+        // Encuentra el componente AdministracionInventario en el CanvasInventario
+        GameObject canvasInventario = GameObject.Find("CanvasInventario");
+        if (canvasInventario != null)
+        {
+            adminInventario = canvasInventario.GetComponent<AdministracionInventario>();
+        }
     }
 
-    
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Time.timeScale = 1;
+            MenuCultivos.SetActive(false);
+            InterfazCultivos = false;
+
+            // Actualiza el estado en AdministracionInventario
+            if (adminInventario != null)
+            {
+                adminInventario.InterfazCultivos = false;
+            }
+        }
+    }
+
+    public void VolverMenu()
+    {
+        // Desactiva el menú de cultivos
+        MenuCultivos.SetActive(false);
+        InterfazCultivos = false;
+
+        // Encuentra el menú de inventario y lo activa
+        GameObject padre = GameObject.Find("CanvasInventario");
+        if (padre != null)
+        {
+            Transform hijoTransform = padre.transform.Find("MenuInventario");
+            if (hijoTransform != null)
+            {
+                GameObject hijo = hijoTransform.gameObject;
+                hijo.SetActive(true); // Activa el inventario
+
+                // Actualiza el estado en AdministracionInventario
+                if (adminInventario != null)
+                {
+                    adminInventario.InterfazCultivos = false;
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Hijo no encontrado");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Padre no encontrado");
+        }
     }
 }
+
--- Assets/Inventario/AdministracionInventario.cs
+++ Assets/Inventario/AdministracionInventario.cs
@@ -5,48 +5,67 @@ using UnityEngine;
 public class AdministracionInventario : MonoBehaviour
 {
     public GameObject MenuInventario;
-    private bool ActivacionInventario;
+    public bool ActivacionInventario;
+    public bool InterfazCultivos;
+    public AdministracionCultivos adminCultivos;
+
     void Start()
     {
-        
+        // Encuentra el componente AdministracionCultivos en el CanvasCultivos
+        GameObject canvasCultivos = GameObject.Find("CanvasCultivos");
+        if (canvasCultivos != null)
+        {
+            adminCultivos = canvasCultivos.GetComponent<AdministracionCultivos>();
+        }
     }
 
-    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab) && !ActivacionInventario) 
+        if (Input.GetKeyDown(KeyCode.Tab) && !ActivacionInventario && !InterfazCultivos)
         {
             Time.timeScale = 0;
             MenuInventario.SetActive(true);
             ActivacionInventario = true;
         }
-
         else if (Input.GetKeyDown(KeyCode.Tab) && ActivacionInventario)
         {
             Time.timeScale = 1;
-            MenuInventario.SetActive(false);    
+            MenuInventario.SetActive(false);
             ActivacionInventario = false;
         }
     }
 
-    public void ActivacionInterfaz(string NombreInterfaz)
-    {
-        MenuInventario.SetActive(false);
-        ActivacionInventario = false;
-        GameObject Interfaz = GameObject.Find(NombreInterfaz);
-        if (Interfaz != null)
-        {
-            Interfaz.SetActive(true);
-        }
-
-    }
     public void ActivarCultivos()
     {
-        ActivacionInterfaz("MenuCultivos"); // Asegúrate de usar el nombre correcto
-    }
+        // Desactiva el menú de inventario
+        MenuInventario.SetActive(false);
+        ActivacionInventario = false;
 
-    public void ActivarTienda()
-    {
-        ActivacionInterfaz("MenuTienda"); // Asegúrate de usar el nombre correcto
+        // Encuentra el CanvasCultivos y activa el menú
+        GameObject padre = GameObject.Find("CanvasCultivos");
+        if (padre != null)
+        {
+            Transform hijoTransform = padre.transform.Find("MenuCultivos");
+            if (hijoTransform != null)
+            {
+                GameObject hijo = hijoTransform.gameObject;
+                hijo.SetActive(true); // Activa el menú de cultivos
+
+                // Actualiza el estado en ambos scripts
+                InterfazCultivos = true;
+                if (adminCultivos != null)
+                {
+                    adminCultivos.InterfazCultivos = true;
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Hijo no encontrado");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Padre no encontrado");
+        }
     }
 }
