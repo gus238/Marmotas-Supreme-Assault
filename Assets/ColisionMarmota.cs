2e44652cc6197e6656e5b2e2c0d5796ce880dec8
tintin01234 <augusto16morini@gmail.com>
2024-10-29 18:35:06 -0300

    Script TiempoMuerteOwO

---
+++ Assets/ColisionMarmota.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionMarmota : MonoBehaviour
+{
+    public LayerMask Marmota;
+    public float tiempo = 5.0f;
+    private float tiempoMuerte = 0f; 
+
+    void OnCollisionStay(Collision collision)
+    {
+        if ((Marmota.value & (1 << collision.gameObject.layer)) != 0)
+        {
+          tiempoMuerte += Time.deltaTime;
+          if (tiempoMuerte >= tiempo)
+            {
+              Debug.Log("PERDISTE");
+              tiempoMuerte = 0f; 
+            }
+        }
+        else
+        {
+          tiempoMuerte = 0f;
+        }
+    }
+}
+
+
