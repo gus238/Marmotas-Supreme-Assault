f5564153f0904ae1fa4fcafe94d65079a2fec4d9
gus238 <gus.gilardoni@hotmail.com>
2024-11-22 22:38:51 -0300

    Sonido recoger MONEDA

--- Assets/Moneda.cs
+++ Assets/Moneda.cs
@@ -6,6 +6,8 @@ public class Moneda : MonoBehaviour
 {
     public int valorMoneda = 1;  // Cu�ntas monedas da esta moneda
     public GameObject jugador;  // El objeto del jugador, que puedes arrastrar desde el Inspector
+    public AudioSource sourceSonido;
+    public AudioClip ruiditoMoneda;
     economiaJugador monedas;
 
     void Start()
@@ -19,6 +21,7 @@ public class Moneda : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             monedas.RecibirMonedas(valorMoneda);  // Suma las monedas al jugador
+            sourceSonido.PlayOneShot(ruiditoMoneda);
             Destroy(gameObject);  // Destruye la moneda despu�s de que el jugador la recoge
         }
     }
