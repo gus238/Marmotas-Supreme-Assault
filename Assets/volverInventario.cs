15ee9104bde7441b5f1e42d09fe880a4e1f621f3
gus238 <gus.gilardoni@hotmail.com>
2024-12-12 15:25:26 -0300

    Test

--- Assets/volverInventario.cs
+++ Assets/volverInventario.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class volverInventario : MonoBehaviour
 {
+    //Variables que toman los gameobjects que se tienen que cerrar y abrir cuando apretas volver en cualquier pestaña de la tienda
     public GameObject inventario;
     public GameObject panelArmas;
     public GameObject panelConsumibles;
     public GameObject panelHabilidades;
+
+    //Funcion que se llama al tocar el boton volver en cualquier pestaña de la tienda
     public void PasarInventario()
     {
         inventario.SetActive(true);
